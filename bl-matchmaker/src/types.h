c79f62a766db54c9914276120914f026e35cd64a
Electrk <Electrk@ymail.com>
2022-07-29 17:29:44 -0700

    Add `BITS_X` defines for number of bits in an integer type

--- bl-matchmaker/src/types.h
+++ bl-matchmaker/src/types.h
@@ -18,4 +18,12 @@ typedef unsigned int uint;
 typedef float F32;
 typedef double F64;
 
+#define BITS_U8 (sizeof (U8) << 3)
+#define BITS_U16 (sizeof (U16) << 3)
+#define BITS_U32 (sizeof (U32) << 3)
+
+#define BITS_S8 (sizeof (S8) << 3)
+#define BITS_S16 (sizeof (S16) << 3)
+#define BITS_S32 (sizeof (S32) << 3)
+
 #endif
